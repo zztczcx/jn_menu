2b3bb5be036e132da4bb3dbdb7a655d5cffbefdd
chenxu <zztczcx@163.com>
2016-06-04 00:56:18 +1000

    remove old code

--- app/models/menu_item.rb
+++ app/models/menu_item.rb
@@ -1,20 +1,4 @@
 class MenuItem < ActiveRecord::Base
   belongs_to :menu_page
   belongs_to :dish
-
-  def self.clean_data(origin)
-    origin = origin.compact
-    if origin['id'] then  origin['id'] = origin['id'].to_i end
-    if origin['menu_page_id'] then origin['menu_page_id'] = origin['menu_page_id'].to_i end
-    if origin['price'] then origin['price'] = origin['price'].to_f end
-    if origin['high_price'] then origin['high_price'] = origin['price'].to_f end
-
-    if origin['created_at'] then origin['created_at'] = DateTime.strptime(origin['created_at'], "%Y-%m-%d %H:%M:%S") end
-    if origin['updated_at'] then origin['updated_at'] = DateTime.strptime(origin['updated_at'], "%Y-%m-%d %H:%M:%S") end
-
-    if origin['xpos'] then origin['xpos'] = origin['xpos'].to_f end
-    if origin['ypos'] then origin['ypos'] = origin['ypos'].to_f end
-
-    origin
-  end
 end
--- app/models/menu_page.rb
+++ app/models/menu_page.rb
@@ -3,12 +3,4 @@ class MenuPage < ActiveRecord::Base
   has_many :dishes, through: :menu_items
 
   belongs_to :menu
-
-  def self.clean_data(origin)
-    origin.each do |k, v|
-      if k != 'uuid' 
-        origin[k] = v.to_i
-      end
-    end
-  end
 end
